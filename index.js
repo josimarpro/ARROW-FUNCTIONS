4ebfc06986fd0d729181d3f03db7ad92a26e8191
josimar ferreira <josima-tamires@hotmail.com>
2024-07-04 16:35:42 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let resultadoDAsoma = (numero1,numero2)=>{
+    return numero1 + numero2
+}
+
+console.log(resultadoDAaoma(12,5))
+
+const incentivarQueste = (nomeQuester => {
+    console.log('Parabéns' + nomeQuester = ',voce chegou ao modulo de JS intermediario')
+}
+incentivarQueste('Josimar')
